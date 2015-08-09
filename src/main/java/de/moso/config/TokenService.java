e14207f8d22d4bfd409c20126b720392ff7cdb01
sandro <sandro.sole@gmail.com>
2015-08-09 17:46:19 +0200

    small fixes

--- src/main/java/de/moso/config/TokenService.java
+++ src/main/java/de/moso/config/TokenService.java
@@ -13,13 +13,13 @@ import java.util.UUID;
 public class TokenService {
 
     private static final Logger logger = LoggerFactory.getLogger(TokenService.class);
-    private static final Cache restApiAuthTokenCache = CacheManager.getInstance().getCache("restApiAuthTokenCache");
+    private static final Cache restShopApiAuthTokenCache = CacheManager.getInstance().getCache("restShopApiAuthTokenCache");
     public static final int HALF_AN_HOUR_IN_MILLISECONDS = 30 * 60 * 1000;
 
     @Scheduled(fixedRate = HALF_AN_HOUR_IN_MILLISECONDS)
     public void evictExpiredTokens() {
         logger.info("Evicting expired tokens");
-        restApiAuthTokenCache.evictExpiredElements();
+        restShopApiAuthTokenCache.evictExpiredElements();
     }
 
     public String generateNewToken() {
@@ -27,14 +27,14 @@ public class TokenService {
     }
 
     public void store(String token, Authentication authentication) {
-        restApiAuthTokenCache.put(new Element(token, authentication));
+        restShopApiAuthTokenCache.put(new Element(token, authentication));
     }
 
     public boolean contains(String token) {
-        return restApiAuthTokenCache.get(token) != null;
+        return restShopApiAuthTokenCache.get(token) != null;
     }
 
     public Authentication retrieve(String token) {
-        return (Authentication) restApiAuthTokenCache.get(token).getObjectValue();
+        return (Authentication) restShopApiAuthTokenCache.get(token).getObjectValue();
     }
 }
--- src/main/scala/de/moso/repository/Repository.scala
+++ src/main/scala/de/moso/repository/Repository.scala
@@ -2,10 +2,15 @@ package de.moso.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
 import de.moso.entity._
+
+import collection.JavaConversions._
 /**
  * Created by Sandro on 08.08.15 for shop.
  */
 trait ProductRepository extends MongoRepository[Product, String] {
+
+  //import collection.mutable._
+
   def findByName(name: String): java.util.List[Product]
 
 }
