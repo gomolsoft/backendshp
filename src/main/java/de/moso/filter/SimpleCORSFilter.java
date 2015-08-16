01b403f413f4b13003f637893ed6bd7eac5dbca0
sandro <sandro.sole@gmail.com>
2015-08-16 17:13:57 +0200

    small fixes

--- src/main/java/de/moso/filter/SimpleCORSFilter.java
+++ src/main/java/de/moso/filter/SimpleCORSFilter.java
@@ -13,7 +13,7 @@ public class SimpleCORSFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000" /*"*"*/);
+        response.setHeader("Access-Control-Allow-Origin", /*"http://localhost:3000"*/ "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Username, X-Auth-Password, X-Auth-Token, X-Auth-SmartThings") ;
